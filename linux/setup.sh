861a9fed591d90fc7695813510101a72cbb9cf50
Kevin Gomez <kevingomez2017@gmail.com>
2025-02-11 23:28:21 -0600

    Revert "added aliases"
    This reverts commit 870ed7c634c4b70d7fe438757ee9829b63d7eb17.

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
 #Github aliases
-setupGithubAliases() {
-    if [ -x "$(command -v git)" ]; 
-    then        
-        printf 'Setting up Github aliases...\n'
-        printf "git ac <message> adds all files and commits them.\n"
-        git config --global alias.ac '!f() { git add -A && git commit -m "$1"; }; f'
-        printf "git editcommit <message> edits the previous commit message. You can also add staged \n"
-        git config --global alias.editcommit '!f() { if [ ! -z "$1" ] ; then git commit --amend -m $1; else git commit --amend --no-edit; fi }; f'
-    else
-        printf 'Git is not installed on this machine or current user [%s] does not have permission to execute...\n' "$(whoami)"
-    fi
-}
-# Remember that aliases are session based. Will need to recreate these in a different session or add to .bashrc.
-setupShellAliases() {
-    printf "Placeholder.\n";
-}
-while getopts "gs" var; do
+while getopts ":g" var; do
     case "${var}" in
-        g)
-            setupGithubAliases
-            ;;
         s)
-            setupShellAliases
+            echo "Enabling  "
             ;;
         *)
+            echo "error"
             exit 1;
             ;;
     esac
 done
+
+setupGithubAliases() {
+    if ! [ -x command -v git]
+    then
+        printf "Git is not installed on this machine or current user does not have permission to execute."
+    else
+        git --global alias.ac "!f(){git add -A && git commit -m ${1}};f;"
+}
